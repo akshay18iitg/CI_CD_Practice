561c985c9656e2ab31f3118baea2c7096e1ade9d
akshay18iitg <50731752+akshay18iitg@users.noreply.github.com>
2024-07-06 02:25:31 +0000

    First Commit

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,26 @@
+import requests
+import time
+
+def ping_url(url,delay,max_trials):
+    trials = 0
+    while(trials < max_trials):
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+        else:
+            time.sleep(delay)
+            trials+=1
+    return False
+
+def run():
+    if ping_url(INPUT_URL,INPUT_DELAY,INPUT_MAX_TRIALS) == False:
+        raise Exception
+
+    
+
+
+            
+
+
 if __name__ == '__main__':
-    print('Hello World')
+    run()
