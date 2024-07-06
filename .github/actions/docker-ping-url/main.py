2e352fe17d8fecb1a14594f6567b5d44af9f2e2c
akshay18iitg <50731752+akshay18iitg@users.noreply.github.com>
2024-07-06 02:58:07 +0000

    First Commit

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -4,7 +4,7 @@ import os
 
 def set_output(file_path, key, value):
     with open(file_path,'a') as f:
-        print(f"{key}-{value}", file=f)
+        print(f'{key}-{value}', file=f)
 
 
 def ping_url(url,delay,max_trials):
@@ -24,7 +24,7 @@ def run():
     max_trials = int(os.getenv('INPUT_MAX_TRIALS'))
 
 
-    website_reachable = ping_url(url,delay,max_trials) == False
+    website_reachable = ping_url(url,delay,max_trials)
     set_output(os.getenv('GITHUB_OUTPUT'),'url-reachable',website_reachable)
     if not website_reachable:
         raise Exception
